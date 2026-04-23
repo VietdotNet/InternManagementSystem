a295dc830a5cb87a5d3c7a898f212874e44c8752
vnq03 <vietnq021103@gmail.com>
2026-04-24 00:53:34 +0700

    update gitignore

---
+++ IMS_BE/IMS.Infrastructure/Persistence/SeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Infrastructure.Persistence
+{
+    public static class SeedData
+    {
+        public static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roles = { "Admin", "Mentor", "Intern" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        public static async Task SeedAdmin(UserManager<AppUser> userManager)
+        {
+            var adminEmail = "admin@gmail.com";
+
+            var admin = await userManager.FindByEmailAsync(adminEmail);
+
+            if (admin == null)
+            {
+                admin = new AppUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    FullName = "System Admin"
+                };
+
+                await userManager.CreateAsync(admin, "Admin@123");
+                await userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
+    }
+}
